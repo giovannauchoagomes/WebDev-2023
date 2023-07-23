0dd1ebe7c00fe8361b8e8f25b441a26960e6dc06
giovannauchoagomes <gio-uchoa@hotmail.com>
2023-07-23 15:16:25 -0300

    Primeiro commit

---
+++ edit.js
@@ -0,0 +1,27 @@
+function add(first, second){
+	return first + second;
+}
+
+function sub(first, second){
+	return first - second;
+}
+
+
+function mul(first, second){
+	return first*second;
+}
+
+function div(first, second){
+	return first/second;
+}
+
+function is_even(number){
+	if(number % 2 == 1){
+		return false;
+	}
+	else{
+		return true;
+	}
+}
+
+alert(add(1,2))
