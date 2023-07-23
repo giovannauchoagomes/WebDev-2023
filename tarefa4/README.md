90987e9d0cb2802d49790c00591040960704f186
giovannauchoagomes <gio-uchoa@hotmail.com>
2023-07-23 15:20:24 -0300

    #2 commit

---
+++ tarefa4/README.md
@@ -0,0 +1,4 @@
+# Tarefa 4
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Giovanna-Uch-a/pen/eYQrNar](https://codepen.io/Giovanna-Uch-a/pen/eYQrNar).
+
---
+++ tarefa4/script.js
@@ -0,0 +1,16 @@
+const body = document.querySelector("body");
+const btn = document.querySelector("#alterna");
+
+btn.addEventListener("click", function() {
+    body.classList.toggle("darkmode");
+    var txt = btn.innerText;
+    if(txt == "Dark Mode"){
+      btn.innerText = 'Light Mode';
+      btn.style.borderColor = "gray";
+      btn.style.backgroundColor = "lightgray";
+      } else{
+       btn.innerText = 'Dark Mode';
+       btn.style.backgroundColor = "gray";
+       btn.style.borderColor = "lightgray";
+      }   
+});
